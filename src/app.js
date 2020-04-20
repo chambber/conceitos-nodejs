a73216f569a57e27ce46b6529dfc288c71b4438d
Rubens Pereira do Nascimento <iorubenspn@gmail.com>
2020-04-19 21:16:34 -0300

    [feat] implement update of repository by id

--- src/app.js
+++ src/app.js
@@ -22,8 +22,30 @@ app.post('/repositories', (request, response) => {
   return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  if (repositoryIndex < 0) {
+    return response
+      .status(400)
+      .json({ error: `The repository with "${id}" was not found` });
+  }
+
+  const { likes } = repositories[repositoryIndex];
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  };
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
